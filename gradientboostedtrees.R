85cfd923e2f16b7f14e0e0692619820373b53c6c
Josiah <josiah.parry@gmail.com>
2016-11-21 09:24:58 -0500

    change file to README

--- gradientboostedtrees.R
+++ gradientboostedtrees.R
@@ -10,26 +10,30 @@ absences <- read.csv("https://raw.githubusercontent.com/IBMPredictiveAnalytics/G
 codebook <- "https://archive.ics.uci.edu/ml/datasets/Student+Performance"
 
 
-
 # copy to spark
 absences_tbl <- copy_to(sc, absences, overwrite = T)
 
-# I want to predict G3 based on pstatus, school, sex, age, studytime, & absences
+# I want to predict average score  based on pstatus, school, sex, age, studytime, & absences
 # pstatus, school, & sex are strings. need to convert to binary
 
 # using basic dplyr
 absences_2_tbl <- absences_tbl %>% mutate(p_live_together = ifelse(absences == "T", 1, 0),
                                        male = ifelse(sex == "M", 1, 0),
-                                       school_GP = ifelse(school == "GP", 1, 0)))
+                                       school_GP = ifelse(school == "GP", 1, 0))
 
 # create the index / binary columns 
 # sdf_mutate = spark data frame mutate allows spark functions 
 ## ft_string_indexer creates dummy variable
   # going to check this using basic dplyr
-absences_tbl <- absences_tbl %>% sdf_mutate(p_live_together = ft_string_indexer("Pstatus"), 
-                                            school_index = ft_string_indexer("school"),
-                                            sex_index = ft_string_indexer("sex"))
 
+# Create average score 
+absences_tbl <- absences_tbl %>% mutate(avg_score = (G1 + G2 + G3)/3)
+
+# create binary using Sparklyr methods
+absences_tbl <-  absences_tbl %>% sdf_mutate(p_live_together = ft_string_indexer("Pstatus"), 
+                                            school_index = ft_string_indexer("school"),
+                                            sex_index = ft_string_indexer("sex")) 
+                                            
 
 # using sparklyr to partition data
 partition_tbl <- sdf_partition(absences_tbl, training = 0.75, test = 0.25, seed = 0)
@@ -43,9 +47,11 @@ gbt_predict <- sdf_predict(gbt, newdata = partition_tbl$test) %>% collect
 
 
 true_val <- partition_tbl$test %>% select(G3)
-
 pred_true <- merge(gbt_predict$prediction, true_val)
 
 # evaluation
 sqrt(mean((pred_true$G3 - pred_true$x)^2))
-#awful model lol
+
+
+
+
--- sparklyr_wine.R
+++ sparklyr_wine.R
@@ -1,4 +1,4 @@
-install.packages("devtools")
+#install.packages("devtools")
 devtools::install_github("rstudio/sparklyr")
 
 # load sparklyr & install spark
